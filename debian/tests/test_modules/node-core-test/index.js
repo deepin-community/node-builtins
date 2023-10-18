3ebb375f4861c74c8a340ed689d8ad18c6dea372
Tianyu Chen <sweetyfish@deepin.org>
2023-10-19 01:00:16 +0800

    feat: update node-builtins to 5.0.1

---
+++ debian/tests/test_modules/node-core-test/index.js
@@ -0,0 +1,8 @@
+// https://github.com/nodejs/node/blob/b476b1b91ef8715f096f815db5a0c8722b613678/lib/test.js
+
+'use strict'
+
+const test = require('./lib/harness')
+
+module.exports = test
+module.exports.test = test
